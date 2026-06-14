b3776e16839c15a009209f399f3ace283240288f
Emre Turan <benemreturan@gmail.com>
2026-06-14 19:30:26 +0300

    Add scene document contract

---
+++ engine/scene/sceneDocument.ts
@@ -0,0 +1,18 @@
+import type { Entity } from "./entity";
+
+export const SCENE_DOCUMENT_SCHEMA_VERSION = 1;
+
+export interface SceneWorldSettings {
+  backgroundColor?: string;
+  ambientColor?: string;
+  ambientIntensity?: number;
+  staticObjectsCastShadow?: boolean;
+  staticObjectsReceiveShadow?: boolean;
+}
+
+export interface SceneDocument {
+  schema: typeof SCENE_DOCUMENT_SCHEMA_VERSION;
+  name: string;
+  entities: Entity[];
+  worldSettings?: SceneWorldSettings;
+}
